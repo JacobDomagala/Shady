4ecdeed95f04708d451082ea753572950a62e6ad
Jakub Domagala <domagala.jacob@gmail.com>
2020-09-08 23:26:06 +0200

    #6 Add RendererAPI and RendererCommand classes

---
+++ src/render/renderer_api.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "vertex_array.hpp"
+
+#include <glm/glm.hpp>
+
+namespace shady::render {
+
+class RendererAPI
+{
+public:
+  enum class API
+  {
+    None = 0,
+    OpenGL = 1
+  };
+
+public:
+  virtual ~RendererAPI() = default;
+
+  virtual void
+  Init() = 0;
+
+  virtual void
+  SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) = 0;
+
+  virtual void
+  SetClearColor(const glm::vec4 &color) = 0;
+
+  virtual void
+  Clear() = 0;
+
+  virtual void
+  DrawIndexed(const std::shared_ptr<VertexArray> &vertexArray, uint32_t indexCount = 0) = 0;
+
+  static API
+  GetAPI();
+
+  static std::unique_ptr<RendererAPI>
+  Create();
+
+private:
+  static inline API s_API = API::OpenGL;
+};
+
+}// namespace shady::render

a6c586b150d5a76fb1d3d29426eddddb397b42af
Jakub Domagala <domagala.jacob@gmail.com>
2020-10-23 18:24:18 +0200

    [#50]: Set ROOT_DIR path in CMake so it's not tied to run directory

--- src/utils/file_manager.hpp
+++ src/utils/file_manager.hpp
@@ -5,16 +5,18 @@
 #include <filesystem>
 #include <string>
 
+
 namespace shady::utils {
 
 class FileManager
 {
  public:
    static inline const std::filesystem::path ROOT_DIR =
-      std::filesystem::current_path().parent_path().parent_path();
+      std::filesystem::path(std::string(CMAKE_ROOT_DIR));
    static inline const std::filesystem::path ASSETS_DIR = ROOT_DIR / "assets";
    static inline const std::filesystem::path TEXTURES_DIR = ASSETS_DIR / "textures";
    static inline const std::filesystem::path SHADERS_DIR = ASSETS_DIR / "shaders";
+   static inline const std::filesystem::path MODELS_DIR = ASSETS_DIR / "models";
 
    enum class FileType
    {

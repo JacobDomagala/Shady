0879d473da76108a6cec6bc02b26c8439d54e4f3
Jakub Domagala <domagala.jacob@gmail.com>
2021-04-03 23:57:40 +0200

    [#10]: Use string_view and perfect forwarding for Logger

--- src/trace/logger.impl.hpp
+++ src/trace/logger.impl.hpp
@@ -7,58 +7,62 @@ namespace shady::trace {
 
 template < typename... Args >
 constexpr void
-Logger::Trace(std::string&& buffer, const Args&... args)
+Logger::Trace(std::string_view buffer, Args&&... args)
 {
-   Log< TYPE::TRACE >(std::move(buffer), args...);
+   Log< TYPE::TRACE >(std::move(buffer), std::forward< Args >(args)...);
 }
 
 template < typename... Args >
 constexpr void
-Logger::Debug(std::string&& buffer, const Args&... args)
+Logger::Debug(std::string_view buffer, Args&&... args)
 {
-   Log< TYPE::DEBUG >(std::move(buffer), args...);
+   Log< TYPE::DEBUG >(std::move(buffer), std::forward< Args >(args)...);
 }
 
 template < typename... Args >
 constexpr void
-Logger::Info(std::string&& buffer, const Args&... args)
+Logger::Info(std::string_view buffer, Args&&... args)
 {
-   Log< TYPE::INFO >(std::move(buffer), args...);
+   Log< TYPE::INFO >(std::move(buffer), std::forward< Args >(args)...);
 }
 
 template < typename... Args >
 constexpr void
-Logger::Warn(std::string&& buffer, const Args&... args)
+Logger::Warn(std::string_view buffer, Args&&... args)
 {
-   Log< TYPE::WARNING >(std::move(buffer), args...);
+   Log< TYPE::WARNING >(std::move(buffer), std::forward< Args >(args)...);
 }
 
 template < typename... Args >
 constexpr void
-Logger::Fatal(std::string&& buffer, const Args&... args)
+Logger::Fatal(std::string_view buffer, Args&&... args)
 {
-   Log< TYPE::FATAL >(std::move(buffer), args...);
+   Log< TYPE::FATAL >(std::move(buffer), std::forward< Args >(args)...);
 }
 
 template < TYPE LogLevel, typename... Args >
 constexpr void
-Logger::Log(std::string&& buffer, const Args&... args)
+Logger::Log(std::string_view buffer, Args&&... args)
 {
    if (LogLevel >= s_currentLogType)
    {
-#if defined(_WIN32)
-      auto hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-      SetConsoleTextAttribute(hConsole, s_typeStyles.at(LogLevel));
+      if constexpr (_WIN32)
+      {
+         auto hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+         SetConsoleTextAttribute(hConsole, s_typeStyles.at(LogLevel));
 
-      fmt::print("[{}]{} {}\n", time::GetTime(), ToString(LogLevel),
-                 fmt::format(std::move(buffer), args...));
+         fmt::print("[{}]{} {}\n", time::GetTime(), ToString(LogLevel),
+                    fmt::format(buffer, std::forward< Args >(args)...));
 
-      // Set the color to white
-      SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
-#else
-      fmt::print(fmt::fg(s_typeStyles.at(LogLevel)), "[{}]{} {}\n", time::GetTime(),
-                 ToString(LogLevel), fmt::format(std::move(buffer), args...));
-#endif
+         // Set the color to white
+         SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+      }
+      else
+      {
+         fmt::print(fmt::fg(s_typeStyles.at(LogLevel)), "[{}]{} {}\n", time::GetTime(),
+                    ToString(LogLevel),
+                    fmt::format(buffer, std::forward< Args >(args)...));
+      }
    }
 }
 

67ecba15fbacc0e2d4248109c02170cb8f5564ba
Jacob Domagala <domagala.jacob@gmail.com>
2025-12-05 10:12:20 +0100

    [#127]: Make sur we compile shaders ONLY when source code changes

--- cmake/compile_shaders.cmake
+++ cmake/compile_shaders.cmake
@@ -12,6 +12,26 @@ function(compile_shader)
         message(FATAL_ERROR "compile_shader: OUTPUT_FILE_NAME argument missing")
     endif()
 
-    execute_process(COMMAND ${Vulkan_GLSLC_EXECUTABLE} "${params_SOURCE_FILE}" -o "${params_OUTPUT_FILE_NAME}")
+    add_custom_command(
+        OUTPUT "${params_OUTPUT_FILE_NAME}"
+        COMMAND ${Vulkan_GLSLC_EXECUTABLE}
+                ${params_DEFINES}
+                "${params_SOURCE_FILE}"
+                -o "${params_OUTPUT_FILE_NAME}"
+        DEPENDS "${params_SOURCE_FILE}"
+        COMMENT "Compiling shader ${params_SOURCE_FILE}"
+        VERBATIM
+    )
 
+    set_property(GLOBAL APPEND PROPERTY SHADY_SHADER_OUTPUTS "${params_OUTPUT_FILE_NAME}")
+endfunction()
+
+function(add_shader_target target_name)
+    get_property(shader_outputs GLOBAL PROPERTY SHADY_SHADER_OUTPUTS)
+
+    if (NOT shader_outputs)
+        message(FATAL_ERROR "add_shader_target: no shaders were registered")
+    endif()
+
+    add_custom_target(${target_name} DEPENDS ${shader_outputs})
 endfunction()

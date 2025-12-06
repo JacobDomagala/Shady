08f17139e50c4dfaf9f824ccac89de40a9e283d9
Jakub Domagala <domagala.jacob@gmail.com>
2025-12-06 10:23:34 +0100

    [#127]: Fix issues with compile shaders

--- cmake/compile_shaders.cmake
+++ cmake/compile_shaders.cmake
@@ -26,12 +26,33 @@ function(compile_shader)
     set_property(GLOBAL APPEND PROPERTY SHADY_SHADER_OUTPUTS "${params_OUTPUT_FILE_NAME}")
 endfunction()
 
-function(add_shader_target target_name)
+function(add_shader_target target_name shaders_path)
+    file(GLOB_RECURSE shader_sources CONFIGURE_DEPENDS
+        "${shaders_path}/*.comp"
+        "${shaders_path}/*.frag"
+        "${shaders_path}/*.geom"
+        "${shaders_path}/*.tesc"
+        "${shaders_path}/*.tese"
+        "${shaders_path}/*.vert"
+    )
+
+    foreach(shader_source IN LISTS shader_sources)
+        compile_shader(
+            SOURCE_FILE "${shader_source}"
+            OUTPUT_FILE_NAME "${shader_source}.spv"
+        )
+    endforeach()
+
     get_property(shader_outputs GLOBAL PROPERTY SHADY_SHADER_OUTPUTS)
 
     if (NOT shader_outputs)
-        message(FATAL_ERROR "add_shader_target: no shaders were registered")
+        message(FATAL_ERROR "add_shader_target: no shaders found in ${shaders_path}")
     endif()
 
     add_custom_target(${target_name} DEPENDS ${shader_outputs})
 endfunction()
+
+set(SHADY_SHADERS_PATH "${PROJECT_SOURCE_DIR}/assets/shaders")
+
+add_shader_target(shaders "${SHADY_SHADERS_PATH}")
+add_dependencies(${PROJECT_NAME} shaders)

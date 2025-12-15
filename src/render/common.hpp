06e998f756bbcd9b62d3b77073aae20b4d984f3e
Jacob Domagala <domagala.jacob@gmail.com>
2025-12-15 12:00:00 +0100

    [#116]: Define two frames in flight

--- src/render/common.hpp
+++ src/render/common.hpp
@@ -33,6 +33,7 @@ VK_CHECK(VkResult result, std::string_view errorMessage)
 static constexpr bool ENABLE_VALIDATION = true;
 static constexpr std::array< const char*, 1 > VALIDATION_LAYERS = {"VK_LAYER_KHRONOS_validation"};
 static constexpr std::array< const char*, 1 > DEVICE_EXTENSIONS = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
+constexpr uint32_t MAX_FRAMES_IN_FLIGHT = 2;
 
 /*
  * This is storage for common Vulkan objects that are needed for numerous
